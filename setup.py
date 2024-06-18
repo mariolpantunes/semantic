7b0c83899c916da24008488415aa27a40afc8b49
Mário Antunes <mariolpantunes@gmail.com>
2024-06-18 08:45:24 +0100

    Update setup.py
    To match the requirements.txt

--- setup.py
+++ setup.py
@@ -26,8 +26,6 @@ setup(
     url='https://github.com/mariolpantunes/semantic',
     packages=find_packages(),
     cmdclass={'download_nltk': DownloadNLTK},
-    install_requires=['nltk>=3.7', 'numpy>=1.22.3', 'requests>=2.27.1', 'scikit-learn>=1.0.2', 'joblib>=1.1.0',
-                      'nmf @ git+https://github.com/mariolpantunes/nmf@main#egg=nmf',
-                      'knee @ git+https://github.com/mariolpantunes/knee@main#egg=knee'],
+    install_requires=['nltk>=3.7', 'numpy>=1.22.3', 'requests>=2.27.1', 'scikit-learn>=1.0.2', 'joblib>=1.1.0', pyNNMF>=0.1.3, kneeliverse>=1.0],
     setup_requires=['nltk>=3.7']
 )
